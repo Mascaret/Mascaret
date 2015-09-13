f57ea9088a14e4329b5acbd1a226f9cf12eeed21
Remi <remiclaeys.utc@gmail.com>
2015-09-13 08:48:48 +0200

    New Classes from Remi

---
+++ database/logical_objects/costElement.py
@@ -0,0 +1,7 @@
+class CostElement:
+    def __init__(self,Id,intitule,description,list_expenses,list_groupCostElement):
+        self.Id = Id
+        self.intitule = intitule
+        self.description = description
+        self.list_expenses = list_expenses
+        self.list_groupCostElement = list_groupCostElement
